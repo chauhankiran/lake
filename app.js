0ed91db59c39c20eaa669d547f2f7fbe0a5699a2
Kiran <kiranchauhan68@gmail.com>
2023-04-05 22:21:21 +0530

    project pages

--- app.js
+++ app.js
@@ -2,10 +2,38 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.use(morgan("tiny"));
 
+// home.
 app.get("/", async (req, res, next) => {
-  res.send("home");
+  res.render("home");
+});
+
+// dashboard.
+app.get("/dashboard", async (req, res, next) => {
+  res.render("dashboard");
+});
+
+// projects.
+app.get("/projects", async (req, res, next) => {
+  res.render("projects");
+});
+
+// add project.
+app.get("/projects/new", async (req, res, next) => {
+  res.render("new-project");
+});
+
+// show project.
+app.get("/projects/:id", async (req, res, next) => {
+  res.render("show-project");
+});
+
+// edit project.
+app.get("/projects/:id/edit", async (req, res, next) => {
+  res.render("edit-project");
 });
 
 const PORT = process.env.PORT || 3000;
